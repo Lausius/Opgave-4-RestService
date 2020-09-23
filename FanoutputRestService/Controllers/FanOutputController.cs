31c7761a674a6d238c7d1b504cc0830c5c6bc8d2
Lausius <skinkens_email@yahoo.dk>
2020-09-23 13:15:14 +0200

    Add project files.

---
+++ FanoutputRestService/Controllers/FanOutputController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Opgave1_Model_Klasse;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FanoutputRestService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FanOutputController : ControllerBase
+    {
+        // GET: api/<FanOutputController>
+        [HttpGet]
+        public IEnumerable<FanOutput> Get()
+        {
+            return FanOutput.fanOutputReadings;
+        }
+
+        // GET api/<FanOutputController>/5
+        [HttpGet("{id}")]
+        public FanOutput Get(int id)
+        {
+            return FanOutput.fanOutputReadings.Find(i => i.Id == id);
+        }
+
+        // POST api/<FanOutputController>
+        [HttpPost]
+        public void Post([FromBody] FanOutput value)
+        {
+            FanOutput.fanOutputReadings.Add(value);
+        }
+
+        // PUT api/<FanOutputController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] FanOutput value)
+        {
+            FanOutput fanOutput = Get(id);
+            if (fanOutput != null)
+            {
+                fanOutput.Id = value.Id;
+                fanOutput.Name = value.Name;
+                fanOutput.Temperature = value.Temperature;
+                fanOutput.Humidity = value.Humidity;
+            }
+        }
+
+        // DELETE api/<FanOutputController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            FanOutput fanOutput = Get(id);
+            FanOutput.fanOutputReadings.Remove(fanOutput);
+        }
+    }
+}
---
+++ ModelLib/FanOutput.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Opgave1_Model_Klasse
+{
+    public class FanOutput
+    {
+        public static List<FanOutput> fanOutputReadings = new List<FanOutput>()
+        {
+            new FanOutput(1, "First Output", 15, 30),
+            new FanOutput(2, "Second Output", 18, 41),
+            new FanOutput(3, "Third Output", 24, 69),
+            new FanOutput(4, "Fourth Output", 22, 49),
+            new FanOutput(5, "Sixth Output", 20, 75),
+            new FanOutput(6, "Seventh Output", 16, 50),
+        };
+
+        public FanOutput()
+        {
+
+        }
+
+        public FanOutput(int id, string name, double temperature, double humidity)
+        {
+            Id = id;
+            Name = name;
+            Temperature = temperature;
+            Humidity = humidity;
+        }
+
+        private int _id;
+        private string _name;
+        private double _temperature;
+        private double _humidity;
+
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                if (value > 0)
+                {
+                    _id = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Id", "Id må ikke være 0 eller negativ.");
+                }
+            }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value.Length >= 2)
+                {
+                    _name = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Navn", "Navn skal mindst indeholde 2 eller flere bogstaver");
+                }
+            }
+        }
+        public double Temperature
+        {
+            get { return _temperature; }
+            set
+            {
+                if (value >= 15 && value <= 25)
+                {
+                    _temperature = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Temperatur", "Temperaturen skal ligge mellem 15 og 25 grader celsius");
+                }
+            }
+        }
+        public double Humidity
+        {
+            get { return _humidity; }
+            set
+            {
+                if (value >= 30 && value <= 80)
+                {
+                    _humidity = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Fugtighed", "Fugtigheden skal have en værdi mellem 30 og 80");
+                }
+            }
+        }
+
+    }
+}
